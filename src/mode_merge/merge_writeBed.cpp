e9a97c4ee75ddce64fc2344872a54a9c1f0b016e
Nikolaos Lykoskoufis <nikolaoslykoskoufis@Nikolaoss-MacBook-Pro.local>
2021-07-01 11:12:23 +0200

    Adding check of genes

--- src/mode_merge/merge_writeBed.cpp
+++ src/mode_merge/merge_writeBed.cpp
@@ -9,14 +9,19 @@ void Merge::writeBed(std::string fout) {
     for (int i=0; i < samples.size(); i++) fdo << "\t" << samples[i]; 
     fdo << std::endl;
 
-    for(int i = 0; i < id.size(); i++){
-        fdo << chrom[i] << "\t";
-        fdo << start[i] << "\t";
-        fdo << stop[i] << "\t";
-        fdo << id[i] << "\t";
-        fdo << info[i] << "\t";
-        fdo << strd[i];
-        for (int s = 0; s < expression.size(); s++) fdo << "\t" << expression[s][i];
+    for(int g = 0; g < id.size(); g++){
+        fdo << chrom[g] << "\t";
+        fdo << start[g] << "\t";
+        fdo << stop[g] << "\t";
+        fdo << id[g] << "\t";
+        fdo << info[g] << "\t";
+        fdo << strd[g];
+        //for (int s = 0; s < expression.size(); s++) fdo << "\t" << expression[s][i];
+        for (int f= 0; f < expression.size(); f++){
+            for (int s=0; s < expression[f][g].size(); s++){
+                fdo << "\t" << expression[f][g][s];
+            }
+        }
         fdo << std::endl;
     }
 }
