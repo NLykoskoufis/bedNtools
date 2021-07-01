9ade0223165d8b15bb0e2b85877489bd84c70062
Nikolaos Lykoskoufis <nikolaoslykoskoufis@Nikolaoss-MacBook-Pro.local>
2021-07-01 10:55:31 +0200

    Adding check of genes

--- src/mode_merge/merge_readPhenotypes.cpp
+++ src/mode_merge/merge_readPhenotypes.cpp
@@ -43,7 +43,7 @@ void Merge::readPhenotypes(string fbed){
                 if (tokens[3] != id[phenotype_count-1]) vrb.error("Files do not contain same genes or are not sorted identically");
             } 
             std::vector < float > values(sample_count, 0.0);
-            for (v = 0; v < sample_count; v++) values[v] = stof(tokens[6+v]);
+            for (int v = 0; v < sample_count; v++) values[v] = stof(tokens[6+v]);
             val.push_back(values);
         }
     }
